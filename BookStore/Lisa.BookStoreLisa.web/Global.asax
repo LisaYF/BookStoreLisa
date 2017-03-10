22ca84db0fc66a538fefdb289304987b0a5bad0a
LisaYF <1603454778@qq.com>
2017-03-10 10:23:57 +0800

    Lisa 创建项目

---
+++ BookStore/Lisa.BookStoreLisa.web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Lisa.BookStoreLisa.web.MvcApplication" Language="C#" %>
---
+++ BookStore/Lisa.BookStoreLisa.web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Lisa.BookStoreLisa.web.Startup))]
+namespace Lisa.BookStoreLisa.web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
